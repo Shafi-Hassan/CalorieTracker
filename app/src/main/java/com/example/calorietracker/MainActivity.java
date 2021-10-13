9f08c6a8795233a28b271ea253c3f66115e0d5e9
Shafi-Hassan <90702011+Shafi-Hassan@users.noreply.github.com>
2021-10-13 18:59:09 -0400

    Intial Commit

---
+++ app/src/main/java/com/example/calorietracker/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.calorietracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.settingsbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
