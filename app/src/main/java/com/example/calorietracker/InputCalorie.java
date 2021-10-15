1260ba0eb2ad684e667663cd63e00f64f361082f
nguyenvt35 <69822014+nguyenvt35@users.noreply.github.com>
2021-10-15 14:22:07 -0400

    Enter Calorie Page Changes
    The layout of the enter calories page has been changed and you can now enter total calories, carbs, fats, and proteins

--- app/src/main/java/com/example/calorietracker/InputCalorie.java
+++ app/src/main/java/com/example/calorietracker/InputCalorie.java
@@ -1,13 +1,32 @@
 package com.example.calorietracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InputCalorie extends AppCompatActivity{
+
+        Button back;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.input_calorie);
+
+             back = findViewById(R.id.finish);
+             back.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View view) {
+                     goBack();
+                 }
+             });
+        }
+
+        private void goBack() {
+            Intent intent = new Intent(this, Daily.class);
+            startActivity(intent);
         }
 }
