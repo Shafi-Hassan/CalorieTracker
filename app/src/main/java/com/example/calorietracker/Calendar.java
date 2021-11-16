63c74fe1e720615a7889e4f748070c62b489be4f
DESKTOP-TT4J41R\pooja <poojapdx@gmail.com>
2021-11-16 16:37:08 -0500

    Fixed deleted file by TA

--- app/src/main/java/com/example/calorietracker/Calendar.java
+++ app/src/main/java/com/example/calorietracker/Calendar.java
@@ -15,8 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Calendar extends MainActivity {
 
-    // Define the variable of CalendarView type
-    // and TextView type;
     CalendarView calendar;
     TextView date_view;
     @Override
@@ -25,36 +23,24 @@ public class Calendar extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
 
-        // By ID we can use each component
-        // which id is assign in xml file
-        // use findViewById() to get the
-        // CalendarView and TextView
+        // get the CalendarView and TextView
         calendar = (CalendarView)
                 findViewById(R.id.calendar);
         date_view = (TextView)
                 findViewById(R.id.date_view);
 
-        // Add Listener in calendar
+        // Listener in calendar
         calendar
                 .setOnDateChangeListener(
                         new CalendarView
                                 .OnDateChangeListener() {
                             @Override
-
-                            // In this Listener have one method
-                            // and in this method we will
-                            // get the value of DAYS, MONTH, YEARS
                             public void onSelectedDayChange(
                                    CalendarView view,
                                     int year,
                                     int month,
                                     int dayOfMonth)
                             {
-
-                                // Store the value of date with
-                                // format in String type Variable
-                                // Add 1 in month because month
-                                // index is start with 0
                                 String Date
                                         = dayOfMonth + "-"
                                         + (month + 1) + "-" + year;
--- app/src/main/java/com/example/calorietracker/MainActivity.java
+++ app/src/main/java/com/example/calorietracker/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.calorietracker;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
