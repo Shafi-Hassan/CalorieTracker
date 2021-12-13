f5716fdf86f71f47cf72d4807798def2cc9ed1ed
blueeyedwolves <55494888+blueeyedwolves@users.noreply.github.com>
2021-12-13 18:49:25 -0500

    trying to get profile feature to work

--- app/src/main/java/com/example/calorietracker/Profile.java
+++ app/src/main/java/com/example/calorietracker/Profile.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Profile extends MainActivity {
 
     Button edit;
+    protected static DailyIntakeStats dailyIntake = MainActivity.dailyIntake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,24 @@ public class Profile extends MainActivity {
         setContentView(R.layout.profile);
 
         getSupportActionBar().setTitle(Html.fromHtml("<font color = \"#30cfd0\">" + getString(R.string.prof) + "</font>"));
+
+        String name = dailyIntake.getName();
+        int age = dailyIntake.getAge();
+        int feet = dailyIntake.getFeet();
+        int inch = dailyIntake.getInch();
+        int weight = dailyIntake.getWeight();
+
+        TextView dis_name = (TextView) findViewById(R.id.dis_name);
+        TextView dis_age = (TextView) findViewById(R.id.dis_age);
+        TextView dis_ft = (TextView) findViewById(R.id.feet);
+        TextView dis_in = (TextView) findViewById(R.id.inches);
+        TextView dis_weight = (TextView) findViewById(R.id.weight);
+
+        dis_name.setText(name);
+        dis_age.setText(age);
+        dis_ft.setText(feet);
+        dis_in.setText(inch);
+        dis_weight.setText(weight);
 
         edit = findViewById(R.id.edit);
 
